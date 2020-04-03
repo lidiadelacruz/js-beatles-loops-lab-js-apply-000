bcc135fb7b5a7ce09673e7b834605d4d2292fef8
beauteous-subroutine-6489 <lidcruz@hotmail.com>
2020-04-03 21:11:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay (arrOne, arrTwo) { 
+  let emptyArray = []
+for (let i = 0, j=0; i<= arrOne.length && j <=arrTwo.length-1; i++,j++){
+  emptyArray.push(arrOne[i]+ " " + 'plays' + " " + arrTwo[j])
+}
+return emptyArray
+}
+
+function iLoveTheBeatles(num){
+  let emptyArray = []  
+do {
+  emptyArray.push(`I love the Beatles!` )
+  num++;
+}
+while (num < 15);
+ return emptyArray
+}
+
